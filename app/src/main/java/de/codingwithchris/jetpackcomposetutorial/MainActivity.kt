d153e08d5f1165d28dbc6812a01026bc8419039d
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2023-07-23 11:00:01 +0200

    Add MyApp Startpoint

--- app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
@@ -3,12 +3,14 @@ package de.codingwithchris.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import de.codingwithchris.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,17 +32,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp()
+{
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeTutorialTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
