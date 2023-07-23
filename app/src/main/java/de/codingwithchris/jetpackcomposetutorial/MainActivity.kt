d13d72994e1ed0e0754a31914271d3f0e84bc687
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2023-07-23 11:21:05 +0200

    Add Basic Functions to Jetpack Compose

--- app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,23 @@ package de.codingwithchris.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import de.codingwithchris.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
@@ -24,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyApp()
+                    MyAppWithStates()
                 }
             }
         }
@@ -34,13 +44,112 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp()
 {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
 
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun MyAppRow()
+{
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun MyAppBox()
+{
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color.Black
+        )
+
+        Text(
+            text = "Hello, Jetpack Compose",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun MyAppWithStates()
+{
+    var counter by remember{
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Counter $counter",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+
+
+        Button(onClick = {
+            counter++
+        }) {
+            Text(text = "Erhöhen")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeTutorialTheme {
-        MyApp()
+        MyAppWithStates()
     }
 }
