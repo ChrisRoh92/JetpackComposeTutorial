ab365e96949d7abba4ac2646a5a2a87250010326
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2023-08-03 15:59:43 +0200

    Compose Function with own State and One with State Hoisting

--- app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,8 +40,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp()
 {
+    // No State Hoisting:
+    SimpleStateComponent()
 
+    // State Hoisting
+    // var counter by remember {
+    //        mutableStateOf(0)
+    // }
+    // HoistedStateComponent(counter = counter, onCounterChange = { newCount ->
+    //        counter = newCount
+    // })
 }
+
+@Composable
+fun SimpleStateComponent()
+{
+    var counter by remember {
+        mutableStateOf(0)
+    }
+    Button(onClick = { counter++ }) {
+        Text("Counter is: $counter")
+    }
+}
+
+@Composable
+fun HoistedStateComponent(counter: Int, onCounterChange: (Int)-> Unit)
+{
+    Button(onClick = { onCounterChange(counter + 1) }) {
+        Text("Counter is: $counter")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
