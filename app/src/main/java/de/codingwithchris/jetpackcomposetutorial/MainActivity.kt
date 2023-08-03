d074bf59ddd759c8f7cf72e1e316f254bf85f88d
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2023-08-03 18:46:48 +0200

    Added Two Scaffolds

--- app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/de/codingwithchris/jetpackcomposetutorial/MainActivity.kt
@@ -3,22 +3,38 @@ package de.codingwithchris.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import de.codingwithchris.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
+            JetpackComposeTutorialTheme(
+            ) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -34,7 +50,74 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp()
 {
+//    SimpleScaffold()
+    ScaffoldWithFabAndSnackBar()
+}
 
+// Scaffold Function
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleScaffold()
+{
+    Scaffold(
+        topBar = { TopAppBar(
+            title = { Text("My App") },
+            actions = {
+                IconButton(onClick = { /* Do Something */ }) {
+                    Icon(Icons.Default.Settings, contentDescription = "Settings Icon ")
+                }
+            }
+        )}
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Text("Hello Simple Scaffold")
+        }
+
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldWithFabAndSnackBar() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        topBar = { TopAppBar(
+            title = { Text("My App") },
+            actions = {
+                IconButton(onClick = { /* Do Something */ }) {
+                    Icon(Icons.Default.Settings, contentDescription = "Settings Icon ")
+                }
+            }
+        )},
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                scope.launch {
+                    snackbarHostState.showSnackbar("Hello User")
+                }
+            }) {
+                Icon(Icons.Default.Add, contentDescription = "Add Button")
+            }
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Hello from Scaffold with Snackbar")
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
